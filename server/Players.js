7384f65f4eb5abc45f81905e99acb9b62cb5696d
fatfisz <fatfisz@gmail.com>
2020-05-07 21:02:40 +0200

    Add basic handling of players

---
+++ server/Players.js
@@ -0,0 +1,21 @@
+exports.Players = class Players {
+  #players = new Map();
+
+  getScores() {
+    return [...this.#players.entries()].sort(
+      ([playerIdA, scoreA], [playerIdB, scoreB]) =>
+        scoreB - scoreA || playerIdA.localeCompare(playerIdB)
+    );
+  }
+
+  addPlayer(playerId) {
+    this.#players.set(playerId, 0);
+  }
+
+  increasePlayerScore(playerId) {
+    if (!this.#players.has(playerId)) {
+      return;
+    }
+    this.#players.set(playerId, this.#players.get(playerId) + 1);
+  }
+};
--- server/Room.js
+++ server/Room.js
@@ -1,7 +1,9 @@
+const { Players } = require('./Players');
 const { Table } = require('./Table');
 
 exports.Room = class Room {
   constructor() {
+    this.players = new Players();
     this.table = new Table();
   }
 };
