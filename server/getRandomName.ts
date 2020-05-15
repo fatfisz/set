0342579d906fdfb024c184bc084fbb330bc282cc
fatfisz <fatfisz@gmail.com>
2020-05-15 18:55:45 +0200

    Use a package for generating random names

--- server/getRandomName.ts
+++ server/getRandomName.ts
@@ -1,24 +1,14 @@
-import axios from 'axios';
+import {
+  uniqueNamesGenerator,
+  adjectives,
+  colors,
+  animals,
+} from 'unique-names-generator';
 
-let anonymousCount = 0;
-
-export async function getRandomName() {
-  try {
-    const response = await axios.get('http://names.drycodes.com/1', {
-      params: {
-        separator: 'space',
-        format: 'text',
-      },
-    });
-
-    if (response.status !== 200) {
-      throw Object.assign(new Error('Could not get the name'), { response });
-    }
-
-    return response.data as string;
-  } catch (error) {
-    console.error(error);
-    anonymousCount += 1;
-    return `Anonymous ${anonymousCount}`;
-  }
+export function getRandomName() {
+  return uniqueNamesGenerator({
+    dictionaries: [adjectives, colors, animals],
+    separator: ' ',
+    style: 'capital',
+  });
 }
